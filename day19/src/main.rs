c0ffb94f2de3b868c5e183e2e366902bdd5ecd84
Erwin Kroon <erwin@k3n.nl>
2020-06-10 18:59:07 +0400

    Day19

---
+++ day19/src/main.rs
@@ -0,0 +1,60 @@
+use scan_fmt::{scan_fmt, scanln_fmt};
+use std::collections::HashSet;
+use std::io::stdin;
+
+const REACTION: &str = "{} => {}";
+
+fn main() {
+    solve();
+}
+
+fn solve() {
+    let mut reactions = Vec::<(String, String)>::new();
+    while let Ok((from, to)) = scanln_fmt!(REACTION, String, String) {
+        reactions.push((from, to));
+    }
+    let mut input = String::new();
+    stdin().read_line(&mut input).expect("NO STRING FOUND");
+    // println!("{:?} {}", reactions, input);
+    let result: HashSet<String> = reactions
+        .iter()
+        .flat_map(|(from, to)| {
+            let mut results = Vec::<String>::new();
+            let mut loop_count = 0;
+            let mut find_from = 0;
+            while let Some(idx) = &input[find_from..].find(from) {
+                loop_count += 1;
+                let mut result = input.clone();
+                result.replace_range((find_from + *idx)..(find_from + *idx + from.len()), to);
+                results.push(result);
+                find_from += idx + from.len();
+            }
+            results
+        })
+        .collect();
+    println!("part 1: {:?}", result.len());
+
+    let mut result = input.clone();
+    let mut steps = 0;
+    let mut replacements_used = Vec::<(String, String, usize)>::new();
+    while &result != "e" {
+        let start = result.clone();
+        for (from, to) in &reactions {
+            let matches = result.matches(to).count();
+            steps += matches;
+            result = result.replace(to, &from);
+            if matches > 0 {
+                replacements_used.push((from.clone(), to.clone(), matches));
+            }
+        }
+        if start == result {
+            result = input.clone();
+            steps = 0;
+            reactions.rotate_left(1);
+            replacements_used = Vec::new();
+        }
+    }
+    println!("part 2: {}", steps);
+    replacements_used.reverse();
+    println!("{:?}", replacements_used);
+}
